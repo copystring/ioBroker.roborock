322e4865b5a08cd724ad8f5a657ec8bb0b5bc1ad
copystring <copystring@gmail.com>
2024-01-15 22:25:05 +0100

    Recreate UserData on reconnect after 3 hours

--- main.js
+++ main.js
@@ -72,19 +72,11 @@ class Roborock extends utils.Adapter {
 
 		await this.setupBasicObjects();
 
-		const username = this.config.username;
-		const password = this.config.password;
-
-		if (!username || !password) {
-			this.log.error("Username or password missing!");
-			return;
-		}
-		await this.setStateAsync("info.connection", { val: true, ack: true });
 
 		// create new clientID if it doesn't exist yet
 		let clientID = "";
-		await this.getStateAsync("clientID").then(async (storedClientID) => {
-			if (storedClientID && typeof storedClientID != "undefined") {
+		this.getStateAsync("clientID").then(async (storedClientID) => {
+			if (storedClientID) {
 				clientID = storedClientID.val?.toString() ?? "";
 			} else {
 				clientID = crypto.randomUUID();
@@ -93,51 +85,31 @@ class Roborock extends utils.Adapter {
 		});
 
 		// Initialize the login API (which is needed to get access to the real API).
-		const loginApi = axios.create({
+		this.loginApi = axios.create({
 			baseURL: "https://euiot.roborock.com",
 			headers: {
-				header_clientid: crypto.createHash("md5").update(username).update(clientID).digest().toString("base64"),
+				header_clientid: crypto.createHash("md5").update(this.config.username).update(clientID).digest().toString("base64"),
 			},
 		});
+
+		if (!this.config.username || !this.config.password) {
+			this.log.error("Username or password missing!");
+			return;
+		}
+		await this.setStateAsync("info.connection", { val: true, ack: true });
 		// api/v1/getUrlByEmail(email = ...)
 
 		// Try to load existing userdata.
 		const userdataObj = await this.getStateAsync("UserData");
 		let userdata;
-		if (userdataObj && typeof userdataObj != "undefined") {
+		if (userdataObj) {
 			userdata = JSON.parse(userdataObj.val?.toString() || "{}");
 		} else {
-			// try log in.
-			userdata = await loginApi
-				.post(
-					"api/v1/login",
-					new URLSearchParams({
-						username: username,
-						password: password,
-						needtwostepauth: "false",
-					}).toString()
-				)
-				.then((res) => res.data.data);
-
-			// Alternative without password:
-			// await loginApi.post("api/v1/sendEmailCode", new url.URLSearchParams({username: username, type: "auth"}).toString()).then(res => res.data);
-			// // ... get code from user ...
-			// userdata = await loginApi.post("api/v1/loginWithCode", new url.URLSearchParams({username: username, verifycode: code, verifycodetype: "AUTH_EMAIL_CODE"}).toString()).then(res => res.data.data);
-
-			if (userdata == null) {
-				this.deleteStateAsync("HomeData");
-				this.deleteStateAsync("UserData");
-				this.log.error("Error! Failed to login. Maybe wrong username or password?");
-				return;
-			}
-			await this.setStateAsync("UserData", {
-				val: JSON.stringify(userdata),
-				ack: true,
-			});
+			userdata = await this.getUserData(this.loginApi);
 		}
 
 		try {
-			loginApi.defaults.headers.common["Authorization"] = userdata.token;
+			this.loginApi.defaults.headers.common["Authorization"] = userdata.token;
 		} catch (error) {
 			this.log.error("Failed to login. Most likely wrong token! Deleting HomeData and UserData. Try again! " + error);
 
@@ -181,7 +153,7 @@ class Roborock extends utils.Adapter {
 
 		// Get home details.
 		try {
-			const homeDetail = await loginApi.get("api/v1/getHomeDetail");
+			const homeDetail = await this.loginApi.get("api/v1/getHomeDetail");
 			if (homeDetail) {
 				const homeId = homeDetail.data.data.rrHomeId;
 
@@ -236,6 +208,7 @@ class Roborock extends utils.Adapter {
 					this.reconnectIntervall = this.setInterval(async () => {
 						this.log.debug("Reconnecting after 3 hours!");
 
+						await this.getUserData(this.loginApi);
 						await this.rr_mqtt_connector.reconnectClient();
 						// this.checkForNewFirmware(duid);
 					}, 10800 * 1000);
@@ -267,6 +240,39 @@ class Roborock extends utils.Adapter {
 		} catch (error) {
 			this.log.error("Failed to get home details: " + error.stack);
 		}
+	}
+
+	async getUserData(loginApi)
+	{
+		// try log in.
+		const userdata = await loginApi
+			.post(
+				"api/v1/login",
+				new URLSearchParams({
+					username: this.config.username,
+					password: this.config.password,
+					needtwostepauth: "false",
+				}).toString()
+			)
+			.then((res) => res.data.data);
+
+		// Alternative without password:
+		// await loginApi.post("api/v1/sendEmailCode", new url.URLSearchParams({username: username, type: "auth"}).toString()).then(res => res.data);
+		// // ... get code from user ...
+		// userdata = await loginApi.post("api/v1/loginWithCode", new url.URLSearchParams({username: username, verifycode: code, verifycodetype: "AUTH_EMAIL_CODE"}).toString()).then(res => res.data.data);
+
+		if (userdata == null) {
+			this.deleteStateAsync("HomeData");
+			this.deleteStateAsync("UserData");
+			this.log.error("Error! Failed to login. Maybe wrong username or password?");
+			return;
+		}
+		await this.setStateAsync("UserData", {
+			val: JSON.stringify(userdata),
+			ack: true,
+		});
+
+		return userdata;
 	}
 
 	async createDevices(products, devices) {
