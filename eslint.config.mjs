fd5fa47fb8e10d08b9916bde2cb486e8682bb4e2
copystring <copystring@gmail.com>
2024-07-13 22:11:22 +0200

    Merge dev (#575)
    * Possible detection fix for isCornerCleanModeSupported
    
    * Fix getCleanSummary
    
    * Add missing comments
    
    * Update S6 MaxV Features
    
    * Update Qrevo MaxV features
    
    * Fix getMap() in getParameter()
    
    * S6 MaxV uses string instead of int cleaning records
    
    * Add carpet mode to Qrevo MaxV
    
    * Add new Qrevo MaxV attributes
    
    * Add new Q revo attribute
    
    * update tsconfig to node18
    
    * update get_status request
    
    * Update S8 states
    
    * Add missing device state
    
    * Add missing semicolon
    
    * Add voice_chat_status to supported robots
    
    * Add connection state to timeouts
    
    * Update README.md
    
    * Fix mqtt connected state
    
    * Fix clean_times & set_switch_status of Qrevo MaxV
    
    * Detect and report unsupported attributes
    
    * Improve camera and voice support detection
    
    * Update README.md
    
    * Fix require of axios
    
    * Fix username and password in io-package.json
    
    * Update package-lock.json
    
    * Wait for tcp client to connect
    
    * Add handling of TCP chunks
    
    * Start websocket after all states are created
    
    * Events are strings not numbers
    
    * Improve start/stop detection in web ui
    
    * Update main.js
    
    * Don't request get_status if websocket isn't connected
    
    * Fix dragging of zones
    
    * Update main.js
    
    * Fix tcp chunkBuffer
    
    * Update localConnector.js
    
    * Add new blocks of Qrevo MaxV
    
    * Improve get_status update algorithm
    
    * Use updateInterval for get_status modulo
    
    * Fix io-package.json
    
    * Update dependencies
    
    * Refactor javascript of web interface
    
    * Don't process map if retry is returned
    
    * Add roborock q revo pro (#574)
    
    * Add Roborock Q Revo Pro
    * Remove MacOS build
    * Update README.md
    
    ---------
    
    Co-authored-by: Stefan Preuß <stefan.preuss.ext@siemens.com>
    
    * Update dependencies
    
    * Update README.md
    
    * Update localConnector.js
    
    * Update dependencies
    
    * Update package-lock.json
    
    * Update package.json
    
    * Update eslint and it's config
    
    * Fix lint
    
    * interpret ./lib/map/zones.js as javascript
    
    * Fix eslint config for zones.js
    
    * Another eslint fix for zones.js
    
    * One more eslint fix for zones.js
    
    * 🙄
    
    * Update eslint.config.mjs
    
    * Update eslint.config.mjs
    
    * Update eslint.config.mjs
    
    * Update eslint.config.mjs
    
    * Update eslint.config.mjs
    
    * Update eslint.config.mjs
    
    * Fix lint in zones.js
    
    * Update eslint.config.mjs
    
    ---------
    
    Co-authored-by: Stefan Preuß <stefan.preuss.ext@siemens.com>

---
+++ eslint.config.mjs
@@ -0,0 +1,40 @@
+import globals from "globals";
+
+export default [
+	{
+		files: ["**/*.js"],
+		ignores: ["**/.eslintrc.js", "admin/words.js", "lib/map/zones.js"],
+		languageOptions: {
+			ecmaVersion: 2020,
+			sourceType: "module",
+			globals: {
+				...globals.node,
+				...globals.mocha,
+			},
+		},
+		rules: {
+			indent: ["error", "tab", { SwitchCase: 1 }],
+			"no-console": "off",
+			"no-unused-vars": ["error", { ignoreRestSiblings: true, argsIgnorePattern: "^_" }],
+			"no-var": "error",
+			"no-trailing-spaces": "error",
+			"prefer-const": "error",
+			quotes: ["error", "double", { avoidEscape: true, allowTemplateLiterals: true }],
+			semi: ["error", "always"],
+		},
+	},
+	{
+		files: ["lib/map/zones.js"],
+		languageOptions: {
+			ecmaVersion: 2020,
+			sourceType: "script",
+			globals: {
+				...globals.browser,
+				...Object.fromEntries(Object.entries(globals.node).map(([key]) => [key, false])),
+			},
+		},
+		rules: {
+			// Specific rules for zones.js can be defined here
+		},
+	},
+];
