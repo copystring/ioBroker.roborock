15e0a0568b4016c0f4842c4e9f81285674fdd4f4
copystring <copystring@gmail.com>
2025-04-28 21:55:03 +0200

    Improve logging

--- main.js
+++ main.js
@@ -1034,9 +1034,9 @@ class Roborock extends utils.Adapter {
 
 		if (error) {
 			if (error.toString().includes("retry") || error.toString().includes("locating") || error.toString().includes("timed out after 30 seconds")) {
-				this.log.warn(`Failed to process ${attribute} on robot ${duid} (${robotModel}): ${error}`);
+				this.log.warn(`Failed to process ${JSON.stringify(attribute)} on robot ${duid} (${robotModel}): ${error}`);
 			} else {
-				this.log.error(`Failed to process ${attribute} on robot ${duid} (${robotModel}): ${error.stack || error}`);
+				this.log.error(`Failed to process ${JSON.stringify(attribute)} on robot ${duid} (${robotModel}): ${error.stack || error}`);
 
 				if (this.supportsFeature && this.supportsFeature("PLUGINS")) {
 					if (this.sentryInstance) {
