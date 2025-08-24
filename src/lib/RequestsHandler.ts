86cd6b171e07d570b802a4541977b112c49fafab
copystring <copystring@gmail.com>
2025-08-24 23:28:31 +0200

    Add new model to MQTT-only L01 device list
    Included 'roborock.vacuum.a101' in the L01Models array for isMQTTOnlyL01Device to support detection of this additional device model.

--- src/lib/RequestsHandler.ts
+++ src/lib/RequestsHandler.ts
@@ -706,7 +706,7 @@ export class RequestsHandler {
 	 * @returns {boolean}
 	 */
 	isMQTTOnlyL01Device(duid) {
-		const L01Models = ["roborock.vacuum.a144", "roborock.vacuum.a147", "roborock.vacuum.a187", "roborock.vacuum.a156", "roborock.vacuum.a135", "roborock.vacuum.a168"];
+		const L01Models = ["roborock.vacuum.a144", "roborock.vacuum.a147", "roborock.vacuum.a187", "roborock.vacuum.a156", "roborock.vacuum.a135", "roborock.vacuum.a168", "roborock.vacuum.a101"];
 
 		const model = this.adapter.http_api.getRobotModel(duid);
 		return typeof model === "string" && L01Models.includes(model.trim());
