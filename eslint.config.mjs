f4f9ef02e6f67c74ce7f9ee3d3c681760f093a22
copystring <copystring@gmail.com>
2025-11-15 17:02:41 +0100

    merge dev_ts to main (#900)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,15 @@
 import globals from "globals";
+import iobroker from "@iobroker/eslint-config";
 
 export default [
 	{
+		// Applies the recommended backend ruleset (Node.js, CommonJS)
+		...iobroker.backend,
+
+		// Applies the recommended frontend ruleset (Browser, ESM)
+		// This configuration is applied ONLY to files inside 'src/www'
+		...iobroker.frontend,
+
 		files: ["**/*.js"],
 		ignores: ["**/.eslintrc.js", "admin/words.js", "build/**", "www/**", "lib/", "test.rrmap.gz"],
 		languageOptions: {
