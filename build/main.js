a65aee3b7b8dba9a4b7768580aba151bf3e88cab
copystring <copystring@gmail.com>
2025-11-29 02:11:32 +0100

    chore: update build artifacts

--- build/main.js
+++ build/main.js
@@ -1,12 +1,38 @@
 "use strict";
 // src/main.ts
 /// <reference types="@iobroker/adapter-core" />
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Roborock = void 0;
-const utils = require("@iobroker/adapter-core");
+const utils = __importStar(require("@iobroker/adapter-core"));
 const crypto_1 = require("crypto");
 const child_process_1 = require("child_process");
-const go2rtc_static_1 = require("go2rtc-static");
+const go2rtc_static_1 = __importDefault(require("go2rtc-static"));
 // --- API & Helper Imports ---
 const roborock_package_helper_1 = require("./lib/roborock_package_helper");
 const requestsHandler_1 = require("./lib/requestsHandler");
