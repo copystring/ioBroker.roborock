48dae7eff6587ae075e2d17be31baa51e9839eaa
copystring <copystring@gmail.com>
2025-12-12 20:01:12 +0100

    Fix: Make builds deterministic and ignore buildInfo.ts (#954)
    Co-authored-by: GitHub Actions Bot <actions@github.com>

--- build/lib/buildInfo.js
+++ build/lib/buildInfo.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.buildInfo = void 0;
 exports.buildInfo = {
-    buildDate: "12.12.2025, 19:46:01",
-    commitHash: "5dbed48",
+    buildDate: "12.12.2025, 20:00:09",
+    commitHash: "31d0729",
 };
 //# sourceMappingURL=buildInfo.js.map
