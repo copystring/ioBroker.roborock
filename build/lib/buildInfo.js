22feefce36e8d97a7bd47ace8e4339b9eb27321d
copystring <1298861+copystring@users.noreply.github.com>
2025-12-13 02:44:38 +0000

    chore(build): update build artifacts [skip ci]

--- build/lib/buildInfo.js
+++ build/lib/buildInfo.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.buildInfo = void 0;
 exports.buildInfo = {
-    buildDate: "13.12.2025, 01:50:55",
-    commitHash: "b0893fcb (local changes)",
+    buildDate: "13.12.2025, 03:44:03",
+    commitHash: "ee6d789e",
 };
 //# sourceMappingURL=buildInfo.js.map
