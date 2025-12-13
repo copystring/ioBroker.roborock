8d2b8de91d0764b133b75fc070187ee2981ee9ad
copystring <copystring@gmail.com>
2025-12-13 23:20:34 +0100

    fix(#960): wrap app_goto_target coordinates in array (#962)

--- src/lib/socketHandler.ts
+++ src/lib/socketHandler.ts
@@ -189,7 +189,7 @@ export class socketHandler {
 		const handler = this.adapter.deviceFeatureHandlers.get(duid);
 		if (!handler) throw new Error(`No handler for DUID ${duid}`);
 
-		await this.adapter.requestsHandler.command(handler, duid, "app_goto_target", points);
+		await this.adapter.requestsHandler.command(handler, duid, "app_goto_target", [points]);
 		return { result: "ok" };
 	}
 
