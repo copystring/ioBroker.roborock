178281e81edb636992c429ae789930cfcb246cf6
copystring <copystring@gmail.com>
2025-12-16 23:31:19 +0100

    fix: prevent hang on cloud-only requests when mqtt offline

--- src/lib/requestsHandler.ts
+++ src/lib/requestsHandler.ts
@@ -160,7 +160,7 @@ class RoborockRequest {
 
 		// this.adapter.log.debug(`duid: ${this.duid}, mqtt: ${mqttConnectionState}, local: ${localConnectionState}, remote: ${remoteConnection}`);
 
-		if (!mqttConnectionState && remoteConnection) {
+		if (!mqttConnectionState && (remoteConnection || this.handler.isCloudRequest(this.duid, this.method))) {
 			const errorMsg = `Cloud connection not available. Not sending for method ${this.method} request!`;
 			this.adapter.log.debug(errorMsg);
 			this.rejectPromise(new Error(errorMsg));
