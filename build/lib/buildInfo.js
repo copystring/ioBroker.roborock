6556f0abf9d392a8788eefbbfdec594c9e73a273
copystring <copystring@gmail.com>
2026-01-04 10:54:23 +0100

    fix: restore build/lib/buildInfo.js

---
+++ build/lib/buildInfo.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.buildInfo = void 0;
+exports.buildInfo = {
+    buildDate: "4.1.2026, 10:49:23",
+    commitHash: "0.6.19",
+};
+//# sourceMappingURL=buildInfo.js.map
